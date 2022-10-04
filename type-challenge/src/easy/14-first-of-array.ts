69da8c81ed0af6f585330f442783b5984e0e3eab
segamiken <ken.s.vollyball@gmail.com>
2022-10-05 08:58:09 +0900

    Answer 14-first-of-array

---
+++ type-challenge/src/easy/14-first-of-array.ts
@@ -0,0 +1,19 @@
+import type { Equal, Expect } from "../../utils/index";
+
+/* my answer */
+type First<T extends any[]> = T extends never[] ? never : T[0];
+
+/* test case */
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+];
+
+type errors = [
+  // @ts-expect-error
+  First<"notArray">,
+  // @ts-expect-error
+  First<{ 0: "arrayLike" }>
+];
