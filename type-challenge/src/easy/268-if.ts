8ee3ecb798dce30ba3fde8a97a4c383c83d5d05b
segamiken <ken.s.vollyball@gmail.com>
2022-11-30 09:15:15 +0900

    Answer 268 if

---
+++ type-challenge/src/easy/268-if.ts
@@ -0,0 +1,13 @@
+import type { Equal, Expect } from "../../utils/index";
+
+/* my answer */
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+/* test case */
+type cases = [
+  Expect<Equal<If<true, "a", "b">, "a">>,
+  Expect<Equal<If<false, "a", 2>, 2>>
+];
+
+// @ts-expect-error
+type error = If<null, "a", "b">;
