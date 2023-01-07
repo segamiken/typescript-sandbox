19dd3a00d145809f1eed693b427259efb90482a9
segamiken <ken.s.vollyball@gmail.com>
2023-01-07 13:13:59 +0900

    Answer 8 reaonly part 2

---
+++ type-challenge/src/medium/8-readonly.ts
@@ -0,0 +1,36 @@
+import type { Alike, Expect } from "../../utils/index";
+
+/* _____________ Your Code Here _____________ */
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P];
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+
+/* _____________ Test Cases _____________ */
+type cases = [
+  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+  Expect<Alike<MyReadonly2<Todo1, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "title" | "description">, Expected>>
+];
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, "title" | "invalid">;
+
+interface Todo1 {
+  title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Todo2 {
+  readonly title: string;
+  description?: string;
+  completed: boolean;
+}
+
+interface Expected {
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
+}
