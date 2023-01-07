0796aa5c23ca7aff8862cefcaa5c9963a0113841
segamiken <ken.s.vollyball@gmail.com>
2023-01-07 13:13:39 +0900

    Add new Type to utils

--- type-challenge/utils/index.d.ts
+++ type-challenge/utils/index.d.ts
@@ -5,3 +5,9 @@ export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
 >() => T extends Y ? 1 : 2
   ? true
   : false;
+
+export type MergeInsertions<T> = T extends object
+  ? { [K in keyof T]: MergeInsertions<T[K]> }
+  : T;
+
+export type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
