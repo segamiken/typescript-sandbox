5895d47b4ef58a7ddd32dc2e2b3dad4d4d6142ad
segamiken <ken.s.vollyball@gmail.com>
2023-01-18 09:47:34 +0900

    Answer 9 deep readonly

---
+++ type-challenge/src/medium/9-deep-readonly.ts
@@ -0,0 +1,66 @@
+import type { Equal, Expect } from "../../utils/index";
+
+/* _____________ Your Code Here _____________ */
+type DeepReadonly<T> = {
+  readonly [P in keyof T]: T[P] extends Record<any, any>
+    ? T[P] extends Function
+      ? T[P]
+      : DeepReadonly<T[P]>
+    : T[P];
+};
+
+/* _____________ Test Cases _____________ */
+type cases = [
+  Expect<Equal<DeepReadonly<X1>, Expected1>>,
+  Expect<Equal<DeepReadonly<X2>, Expected2>>
+];
+
+type X1 = {
+  a: () => 22;
+  b: string;
+  c: {
+    d: boolean;
+    e: {
+      g: {
+        h: {
+          i: true;
+          j: "string";
+        };
+        k: "hello";
+      };
+      l: [
+        "hi",
+        {
+          m: ["hey"];
+        }
+      ];
+    };
+  };
+};
+
+type X2 = { a: string } | { b: number };
+
+type Expected1 = {
+  readonly a: () => 22;
+  readonly b: string;
+  readonly c: {
+    readonly d: boolean;
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true;
+          readonly j: "string";
+        };
+        readonly k: "hello";
+      };
+      readonly l: readonly [
+        "hi",
+        {
+          readonly m: readonly ["hey"];
+        }
+      ];
+    };
+  };
+};
+
+type Expected2 = { readonly a: string } | { readonly b: number };
